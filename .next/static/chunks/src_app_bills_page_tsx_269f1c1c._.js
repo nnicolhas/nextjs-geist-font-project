482babd1ac76910c9504861103b5036db30fd710
nnicolhas <nicojoel.etchegaray@gmail.com>
2025-08-16 02:21:18 -0300

    Add .next/static/chunks/src_app_bills_page_tsx_269f1c1c._.js 1755321677446

---
+++ .next/static/chunks/src_app_bills_page_tsx_269f1c1c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1b64c187._.js",
+  "static/chunks/node_modules_8fb7ae2e._.js"
+],
+    source: "dynamic"
+});
