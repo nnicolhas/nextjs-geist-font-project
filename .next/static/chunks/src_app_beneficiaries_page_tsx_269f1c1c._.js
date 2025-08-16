eb5f13799c2e5dc6f351476cbdc4e8181bcb2b9c
nnicolhas <nicojoel.etchegaray@gmail.com>
2025-08-16 02:21:16 -0300

    Add .next/static/chunks/src_app_beneficiaries_page_tsx_269f1c1c._.js 1755321675205

---
+++ .next/static/chunks/src_app_beneficiaries_page_tsx_269f1c1c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8a94fa06._.js",
+  "static/chunks/node_modules_8fb7ae2e._.js"
+],
+    source: "dynamic"
+});
